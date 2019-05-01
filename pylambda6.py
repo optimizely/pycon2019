a69e362ee871ae2853857da52e313c0c81e7891d
Trent Robbins <trent.robbins@optimizely.com>
2019-05-01 08:48:06 -0700

    Added LAZY_TRUE and LAZY_FALSE stuff to get applicative order evaluation in Python. I think that's applicative order. It's definitely lazy.

--- pylambda6.py
+++ pylambda6.py
@@ -45,6 +45,8 @@ def incr(x):
     '''
     return x + 1 ### illegal in rules
 
+ADD = lambda x: lambda y: y(SUCC)(x)
+MUL = lambda x: lambda y: lambda f: y(x(f))
 
 #
 # END prev definitions
@@ -113,3 +115,61 @@ ISZERO = lambda n: n(lambda f: FALSE)(TRUE)
 ISZERO(ZERO)
 ISZERO(ONE)
 ISZERO(TWO)
+
+def fact(n):
+    ''' factorial
+    '''
+    if n == 0:
+        return 1
+    else:
+        return n*fact(n-1)
+
+# fact(4) returns 24
+
+FACT = lambda n: ISZERO(n)(ONE)(MUL(n)(FACT(PRED(n))))  # max recursion depth
+
+# python evaluates arguments first
+# evaluative order not applicative order? see SICP first section.
+# second path explodes in recursion
+# eagerly evaluates arguments - possible to turn off but not in the way you may think!
+
+#FACT(THREE) # maximum recursion depth exceeded, as expected
+
+# another example of the problem in python
+
+def choose(t, a, b):
+    if t:
+        return a
+    else:
+        return b
+
+# note that this assigns the check to the first positional variable
+choose(a < b, a, b)  # all good
+
+a = 0
+#choose(a != 0, a, 1/a)
+# ZeroDivisionError: integer division or modulo by zero
+
+
+# so lets make choose for lambda calculus
+
+def choose(t, a, b):
+    ''' this pulls us back from the brink
+    '''
+    if t:
+        return a() # evaluate only if needed
+    else:
+        return b()
+
+# this implementation of choose returns 0
+choose(a==0, lambda: a, lambda: 1/a)
+
+# so we need a new implementation of TRUE and FALSE:
+# really not part of lambda calculus, just getting applicative order
+LAZY_TRUE = lambda x: lambda y: x()  # added function call
+LAZY_FALSE = lambda x: lambda y: y()  
+ISZERO = lambda n: n(lambda f: LAZY_FALSE)(LAZY_TRUE) # rewrite for lazy
+
+FACT = lambda n: ISZERO(n)(lambda: ONE)(lambda: MUL(n)(FACT(PRED(n))))
+
+FACT(THREE)(incr)(0)  # return 6 - it worked!
